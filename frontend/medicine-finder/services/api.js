d8c96e60dee9aaafc6b7c7c00b5780897e2fe16c
dev-anurag07 <2311201345@stu.manit.ac.in>
2026-07-16 17:53:11 +0530

    fix inventory route

--- frontend/medicine-finder/services/api.js
+++ frontend/medicine-finder/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL:import.meta.env.VITE_API_URL,
-    withCredentials:true,
+    baseURL:meta.env.VITE_API_URL ,
 })
 
 API.interceptors.request.use((req) => {
